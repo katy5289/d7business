6ce415a3681dc42f9c9f52e1b7fa168e927fc78e
katy5289 <katy@seascapewebdesign.com>
2013-06-07 15:07:52 -0700

    first commit

---
+++ sites/all/themes/swbase/template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "footheme" to match
+ * your subthemes name, e.g. if you name your theme "swbase" then the function
+ * name will be "swbase_preprocess_hook". Tip - you can search/replace
+ * on "footheme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function swbase_preprocess_html(&$vars) {
+}
+function swbase_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function swbase_preprocess_page(&$vars) {
+}
+function swbase_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function swbase_preprocess_node(&$vars) {
+}
+function swbase_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function swbase_preprocess_comment(&$vars) {
+}
+function swbase_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function swbase_preprocess_block(&$vars) {
+}
+function swbase_process_block(&$vars) {
+}
+// */
