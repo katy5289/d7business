66a3e6dd054417710fe187f9b211aaad69c78742
katy5289 <katy@seascapewebdesign.com>
2013-06-07 16:11:00 -0700

    Added starter theme, d7business

---
+++ sites/all/themes/d7business/template.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ * 
+ * Rename each function and instance of "footheme" to match
+ * your subthemes name, e.g. if you name your theme "d7business" then the function
+ * name will be "d7business_preprocess_hook". Tip - you can search/replace
+ * on "footheme".
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function d7business_preprocess_html(&$vars) {
+}
+function d7business_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function d7business_preprocess_page(&$vars) {
+}
+function d7business_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function d7business_preprocess_node(&$vars) {
+}
+function d7business_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function d7business_preprocess_comment(&$vars) {
+}
+function d7business_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function d7business_preprocess_block(&$vars) {
+}
+function d7business_process_block(&$vars) {
+}
+// */
